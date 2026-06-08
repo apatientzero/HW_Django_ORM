7a6960df60cd0adadd8c2ea96068519017130aee
Parviz <apatientzero@gmail.com>
2026-06-08 09:54:38 +0300

    Added info

--- work_with_database/catalog/views.py
+++ work_with_database/catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Phone
+
+
+def index(request):
+    # Получаем все телефоны из базы данных
+    phones = Phone.objects.all()
+
+    # Передаем их в контекст шаблона
+    context = {
+        'phones': phones
+    }
+
+    return render(request, 'catalog/index.html', context)
