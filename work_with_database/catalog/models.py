cdad7c0c2eb2788ebbda7c324e15b5a50acc9e37
Parviz <apatientzero@gmail.com>
2026-06-12 18:13:39 +0300

    Update models.py

--- work_with_database/catalog/models.py
+++ work_with_database/catalog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Phone(models.Model):
     id = models.IntegerField(primary_key=True, verbose_name="ID")
     name = models.CharField(max_length=255, verbose_name="Название")
+    slug = models.SlugField(max_length=255, unique=True, blank=True, null=True, verbose_name="URL") # <--- Added
     image = models.URLField(blank=True, null=True, verbose_name="Изображение")
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
     release_date = models.DateField(verbose_name="Дата выпуска")
