1708e0c86a16ab2297a6c1bc2986ad915a44dd78
Parviz <apatientzero@gmail.com>
2026-06-12 18:18:29 +0300

    Update URL patterns for catalog and product views

--- work_with_database/catalog/urls.py
+++ work_with_database/catalog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.show_catalog, name='catalog'),       # Адрес /catalog/
+    path('<slug>/', views.show_product, name='product'), # Адрес /catalog/<slug>/
 ]
